32c68fd51320970477bae144fbd4a381c48db2f1
NikB97 <35953195+NikB97@users.noreply.github.com>
2018-03-13 16:02:24 +0200

    Update task3.py
    commit2(changed)

--- task3.py
+++ task3.py
@@ -1,17 +1,23 @@
+"""hw2_t3"""
+# pylint: disable=invalid-name
+
+
 def selfDividingNumbers(left, right):
-	def self_dividing(n):
-		for d in str(n):
-			if d == '0' or n % int(d) > 0:
-				return False
-		return True
-	ans = []
-	for n in range(left, right + 1):
-		if self_dividing(n):
-			ans.append(n)
-	return ans 
+    """self div numb"""
+    def self_dividing(n):
+        """self div"""
+        for d in str(n):
+            if d == '0' or n % int(d) > 0:
+                return False
+        return True
+    ans = []
+    for n in range(left, right + 1):
+        if self_dividing(n):
+            ans.append(n)
+    return ans
 
-print ("enter left and right borders of list:") 
-a=(int(input()))  
-b=(int(input()))  
-print(selfDividingNumbers(a,b))
 
+print("enter left and right borders of list:")
+a = (int(input()))
+b = (int(input()))
+print(selfDividingNumbers(a, b))
