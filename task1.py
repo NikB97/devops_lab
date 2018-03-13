833ea805d59ae816fe08cd1ac0c4ea28085f2abc
NikB97 <35953195+NikB97@users.noreply.github.com>
2018-03-13 15:59:35 +0200

    Update task1.py
    commit2(changed)

--- task1.py
+++ task1.py
@@ -1,5 +1,7 @@
-#!/bin/python
-
-print ("enter integers X,Y,Z,N:")
-x, y, z, n = (int(input()) for _ in range(4))
-print ([[a,b,c] for a in range(0,x+1) for b in range(0,y+1) for c in range(0,z+1) if sum([a,b,c]) != n ])
+"""hw2_t1"""
+print("enter integers X,Y,Z,N:")
+X, Y, Z, N = (int(input()) for _ in range(4))
+print([[a, b, c] for a in range(0, X+1)
+       for b in range(0, Y+1)
+       for c in range(0, Z+1)
+       if sum([a, b, c]) != N])
